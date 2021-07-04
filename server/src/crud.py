f7e302358a065188ed5970c9373504e25c6dc77a
HaniNussbaum <haninus106@gmail.com>
2021-07-04 15:04:57 +0300

    updated server files

--- server/src/crud.py
+++ server/src/crud.py
@@ -15,22 +15,6 @@ def get_user_by_email(db: Session, email: str):
     return db.query(models.UserInfo).filter(models.UserInfo.email == email).first()
 
 
-def get_project_name(db: Session, pr_name: str):
-    returned_value = models.create_models(pr_name)
-    if returned_value == pr_name:
-        return returned_value
-
-    name = models.get_class_by_tablename(pr_name)
-    if name is None:
-        return None
-    else:
-        return (
-            db.query(name[pr_name])
-            .filter(name[pr_name].project_name == pr_name)
-            .first()
-        )
-
-
 def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.UserInfo).offset(skip).limit(limit).all()
 
@@ -56,37 +40,22 @@ def create_project_2(db: Session, project: schemas.Project):
     db_project_to_add = models.Project(
         email=project.email, project_name=project.project_name
     )
+
+    user_to_add = get_user_by_email(db, project.email)
+    db_project_to_add.sign_up(user_to_add)
     db.add(db_project_to_add)
     db.commit()
+
     db.refresh(db_project_to_add)
+
+
     return db_project_to_add
 
 
 def update_user_created(db: Session, email: str, project_name: str):
     user_to_update = get_user_by_email(db, email)
-    if user_to_update.created_projects != None:
-        string_to_replace = user_to_update.created_projects + ";" + project_name
-    else:
-        string_to_replace = project_name
     db.query(models.UserInfo).filter(models.UserInfo.email == email).update(
-        {"created_projects": string_to_replace}
     )
-
-
-# def create_project(db: Session, project: schemas.Project):
-#     returned_value = models.create_models(project.project_name)
-#
-#     the_model = models.get_class_by_tablename(project.project_name)
-#     model = the_model[project.project_name]
-#     print(the_model)
-#
-#     if the_model is not None:
-#         db_add_user = model(project_name = project.project_name, email=project.email, web_socket="hardcoded")
-#         db.add(db_add_user)
-#         db.commit()
-#         db.refresh(db_add_user)
-#         return db_add_user
-#
 
 
 def check_username_password(db: Session, user: schemas.UserCreate):
